75ea345b05076a23e6f53f042f6f3158b5cfaf1b
Goran Muminovic <goranmuminovic@gmail.com>
2024-02-15 01:43:51 +0100

    added annotations

--- src/main/java/se/lexicon/workshoplibrary/entity/AppUser.java
+++ src/main/java/se/lexicon/workshoplibrary/entity/AppUser.java
@@ -1,15 +1,12 @@
 package se.lexicon.workshoplibrary.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
-@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 
@@ -20,14 +17,18 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int appUserId;
 
+    @Setter
     private String password;
 
+    @Setter
     @Column(unique = true)
     private String username;
 
     private LocalDate regDate;
 
+    @Setter
     @OneToOne
+    @JoinColumn(name = "details_id")
     private Details details;
 
 }
