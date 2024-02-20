3f749372cba933eceb1fdd8fa7addf640328bd73
Goran Muminovic <goranmuminovic@gmail.com>
2024-02-20 11:46:30 +0100

    findByBookId repository method

--- src/main/java/se/lexicon/workshoplibrary/repository/BookDAO.java
+++ src/main/java/se/lexicon/workshoplibrary/repository/BookDAO.java
@@ -1,7 +1,12 @@
 package se.lexicon.workshoplibrary.repository;
 
 import org.springframework.stereotype.Repository;
+import se.lexicon.workshoplibrary.entity.Book;
 
 @Repository
 public interface BookDAO {
+
+    Book findByBookId (int bookId);
+
+
 }
